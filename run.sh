d2e4d502a129c6bf288ee7ec57ef72509857004f
André Padez <andre.padez@gmail.com>
2015-11-21 19:21:19 +0100

    updated environment variables

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 PACKER_SERVER_PORT=9090 \
 GIT_SPY_PORT=60111 \
-GIT_SPY_API_TOKEN=<OAUTH_TOKEN> \
-GIT_SPY_CALLBACK_URL=<URL> \
-MAIL_MAN_REPO=<owner/repo> \
-MAIL_MAN_BRANCH=<branch> \
+GIT_SPY_API_TOKEN=<GITHUB OAUTH TOKEN> \
+MAIL_MAN_REPO=vigour-io/sentinel-test-private \
+MAIL_MAN_BRANCH=develop-dist \
 node index.js
