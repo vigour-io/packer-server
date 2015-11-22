63ebcb8d7d40ede97d70316af1b968f0eb03877a
André Padez <andre.padez@gmail.com>
2015-11-22 17:47:01 +0100

    made bin

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 PACKER_SERVER_PORT=9090 \
 GIT_SPY_PORT=60111 \
-GIT_SPY_API_TOKEN=<GITHUB OAUTH TOKEN> \
+GIT_SPY_API_TOKEN=86236348c255f3b8b73ad097a79ca938c8419fd6 \
 MAIL_MAN_REPO=vigour-io/sentinel-test-private \
 MAIL_MAN_BRANCH=develop-dist \
-node index.js
+./bin/packer-server
