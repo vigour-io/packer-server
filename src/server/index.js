04e953b3bd05a946697afa83b68303928a5f4b29
Shawn Inder <shawn@vigour.io>
2015-12-10 13:56:47 -0500

    lint

--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,17 @@
 var express = require('express')
-var static = require('./middleware/static')
+var staticFiles = require('./middleware/static')
 var config
 
 var Server = module.exports = {
-	port: undefined,
-	app: undefined,
-	start: function(cfg){
-		config = cfg
-		var port = this.port = config.port
-		return new Promise((resolve, reject) => {
-			var app = Server.app = express()
-			app.use(require('./middleware/static')(config))
-			app.listen(port, resolve)
-		})
-	}
+  port: undefined,
+  app: undefined,
+  start: function (cfg) {
+    config = cfg
+    var port = this.port = config.port
+    return new Promise((resolve, reject) => {
+      var app = Server.app = express()
+      app.use(staticFiles(config))
+      app.listen(port, resolve)
+    })
+  }
 }
