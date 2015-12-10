daab993a60bfa1d74d393b8eabe384e154cf2341
Shawn Inder <shawn@vigour.io>
2015-12-10 15:39:40 -0500

    Add gzipping (closes #11)

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var staticFiles = require('./middleware/static')
+var compress = require('compression')
 var config
 
 var Server = module.exports = {
@@ -10,6 +11,7 @@ var Server = module.exports = {
     var port = this.port = config.port
     return new Promise((resolve, reject) => {
       var app = Server.app = express()
+      app.use(compress())
       app.use(staticFiles(config))
       app.listen(port, resolve)
     })
