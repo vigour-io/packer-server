69c56ea6641ba2823ee6fd5e6c05e7e833432d95
André Padez <andre.padez@gmail.com>
2016-01-21 16:16:05 +0100

    changes

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 PACKER_SERVER_PORT=9090 \
 GIT_SPY_PORT=60111 \
-GIT_SPY_API_TOKEN=gihuboauthtoken \
+GIT_SPY_API_TOKEN=a6bceb1c7aa8add7f3c7c3b82fe797825d84441f \
 MAIL_MAN_REPO=vigour-io/sentinel-test-private \
 MAIL_MAN_BRANCH=develop-dist \
-packer-server
+npm start
