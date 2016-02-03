d4fdf9d101498ba65812e204bee1dae99c863ffc
Shawn Inder <shawn@vigour.io>
2016-02-03 10:16:15 +0100

    ignore run scripts

--- run.sh
+++
@@ -1,8 +0,0 @@
-PACKER_SERVER_PORT=9090 \
-GIT_SPY_PORT=60111 \
-GIT_SPY_API_TOKEN=a6bceb1c7aa8add7f3c7c3b82fe797825d84441f \
-MAIL_MAN_REPO=vigour-io/sentinel-test-private \
-MAIL_MAN_BRANCH=develop-dist \
-MAIL_MAN_GITHUB_USERNAME=vigourmachines \
-MAIL_MAN_GITHUB_PASSWORD=schaap99 \
-npm start
