dd53336fd52363267ca3954fce693cdd1f1c2132
Shawn Inder <shawn@vigour.io>
2016-02-09 13:54:32 +0100

    allow more time for packer to start during tests

--- test/node/updates.js
+++ test/node/updates.js
@@ -15,7 +15,7 @@ var packer
 
 describe('config', () => {
   before(function () {
-    this.timeout(5000)
+    this.timeout(10000)
     return cleanup()
       .then(() => {
         return fs.readJSONAsync(path.join(__dirname, '..', '..', 'configs', 'github.json'))
